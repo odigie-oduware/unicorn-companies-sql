faac002cf0afe0a50c2b4e84e13a53475e01f972
odigie-oduware <odigieoduware16@gmail.com>
2024-08-06 16:09:05 +0100

    Add files via upload

---
+++ data_cleaning.sql
@@ -0,0 +1,149 @@
+/*
+****************************************************************
+				DATA CLEANING
+****************************************************************
+*/
+
+-- Creating the Database
+CREATE DATABASE unicorn_companies_db;
+USE unicorn_companies_db;
+
+-- Creating the Table
+CREATE TABLE unicorn_companies (
+    company VARCHAR(255),
+    valuation VARCHAR(50),
+    date_joined DATE,
+    industry VARCHAR(255),
+    city VARCHAR(255),
+    country VARCHAR(255),
+    continent VARCHAR(255),
+    year_founded INT,
+    funding VARCHAR(50),
+    select_investors TEXT
+);
+-- Importing the data using the MySQL Table Data Import Wizard
+
+-- Inspect the Table
+SELECT * 
+FROM unicorn_companies;
+
+-- Create a new table and populate it with the data from unicorn_companies
+CREATE TABLE unicorns AS
+SELECT *
+FROM unicorn_companies;
+
+-- Inspect the data
+SELECT * FROM unicorns LIMIT 10;
+
+-- Check for missing values
+WITH missing_values AS (
+    SELECT *
+    FROM unicorns
+    WHERE company IS NULL OR company = ''
+       OR valuation IS NULL OR valuation = ''
+       OR date_joined IS NULL
+       OR industry IS NULL OR industry = ''
+       OR city IS NULL OR city = ''
+       OR country IS NULL OR country = ''
+       OR continent IS NULL OR continent = ''
+       OR year_founded IS NULL OR year_founded = ''
+       OR funding IS NULL OR funding = ''
+)
+SELECT * FROM missing_values;
+-- Some records had missing cities (16 records)
+
+-- Fill the missing cities with placeholders 'Unknown'
+UPDATE unicorns
+SET city = 'Unknown'
+WHERE city IS NULL OR city = '';
+
+-- Verify there are no more missing values
+WITH missing_values AS (
+    SELECT *
+    FROM unicorns
+    WHERE company IS NULL OR company = ''
+       OR valuation IS NULL OR valuation = ''
+       OR date_joined IS NULL
+       OR industry IS NULL OR industry = ''
+       OR city IS NULL OR city = ''
+       OR country IS NULL OR country = ''
+       OR continent IS NULL OR continent = ''
+       OR year_founded IS NULL OR year_founded = ''
+       OR funding IS NULL OR funding = ''
+)
+SELECT * FROM missing_values;
+
+-- Check for duplicate records
+WITH duplicates AS (
+    SELECT *,
+           ROW_NUMBER() 
+           OVER (PARTITION BY company, 
+           valuation, date_joined, industry, 
+           city, country, continent, year_founded, 
+           funding, select_investors ORDER BY company) AS duplicate_records
+    FROM unicorns
+)
+SELECT *
+FROM duplicates
+WHERE duplicate_records > 1; 
+
+-- Removing white spaces
+UPDATE unicorns
+SET company = TRIM(company),
+    valuation = TRIM(valuation),
+    date_joined = TRIM(date_joined),
+    industry = TRIM(industry),
+    city = TRIM(city),
+    country = TRIM(country),
+    continent = TRIM(continent),
+    funding = TRIM(funding),
+    select_investors = TRIM(select_investors);
+    
+SELECT *
+FROM unicorns;
+
+-- Add columns for numeric funding and numeric valuation
+ALTER TABLE unicorns
+ADD COLUMN numeric_funding BIGINT,
+ADD COLUMN numeric_valuation BIGINT;
+
+-- Populate numeric_funding
+UPDATE unicorns
+SET numeric_funding = CASE
+    WHEN funding LIKE '$%M' THEN CAST(REPLACE(REPLACE(funding, '$', ''), 'M', '') AS UNSIGNED) * 1000000
+    WHEN funding LIKE '$%B' THEN CAST(REPLACE(REPLACE(funding, '$', ''), 'B', '') AS UNSIGNED) * 1000000000
+    ELSE NULL
+END;
+
+-- Populate numeric_valuation
+UPDATE unicorns
+SET numeric_valuation = CASE
+    WHEN valuation LIKE '$%M' THEN CAST(REPLACE(REPLACE(valuation, '$', ''), 'M', '') AS UNSIGNED) * 1000000
+    WHEN valuation LIKE '$%B' THEN CAST(REPLACE(REPLACE(valuation, '$', ''), 'B', '') AS UNSIGNED) * 1000000000
+    ELSE NULL
+END;
+
+-- Verify that the new columns contain expected values
+SELECT company, funding, numeric_funding, valuation, numeric_valuation
+FROM unicorns
+LIMIT 10;
+
+-- Standardize date formats
+UPDATE unicorns
+SET date_joined = STR_TO_DATE(date_joined, '%Y-%m-%d')
+WHERE date_joined IS NOT NULL;
+
+-- Standardize casing (convert to upper case for consistency) 
+UPDATE unicorns
+SET company = UPPER(company),
+    industry = UPPER(industry),
+    city = UPPER(city),
+    country = UPPER(country),
+    continent = UPPER(continent),
+    select_investors = UPPER(select_investors);
+
+SELECT *
+FROM unicorns
+LIMIT 10;
+
+
---
+++ data_exploration.sql
@@ -0,0 +1,108 @@
+/*
+************************************************************************
+					EXPLORATORY DATA ANALYSIS 
+*************************************************************************
+*/
+
+USE unicorn_companies_db;
+
+-- Inspect the data
+SELECT *
+FROM unicorns;
+
+-- Count the number of rows
+SELECT COUNT(*) AS total_rows FROM unicorns;
+-- There are 1074 records
+
+-- Count the number of distinct industries
+SELECT COUNT(DISTINCT(industry))
+FROM unicorns;
+-- There are 15 different industries in the dataset
+
+-- Calculate the average time it takes for companies to reach unicorn status
+SELECT AVG(YEAR(date_joined) - year_founded) AS avg_years_to_unicorn
+FROM unicorns;
+-- It takes about 7 years for companies to achieve unicorn status from their founding date
+
+-- Determine the industries with the highest number of unicorn companies
+SELECT industry, COUNT(*) AS company_count
+FROM unicorns
+GROUP BY industry
+ORDER BY company_count DESC
+LIMIT 10;
+-- Fintech, Internet Software and E-commerce 
+
+-- Identify the country with the most unicorn companies
+SELECT country, COUNT(*) AS company_count
+FROM unicorns
+GROUP BY country
+ORDER BY company_count DESC
+LIMIT 10;
+-- United States 
+
+-- Identify the city with the most unicorn companies
+SELECT city, COUNT(*) AS company_count
+FROM unicorns
+GROUP BY city
+ORDER BY company_count DESC
+LIMIT 10;
+-- San Fansisco 
+
+-- Find out which investors have funded the most unicorn companies
+WITH investor_list AS (
+    SELECT TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(select_investors, ',', numbers.n), ',', -1)) AS investor
+    FROM unicorns
+    JOIN (SELECT 1 n UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) numbers 
+    ON CHAR_LENGTH(select_investors) - CHAR_LENGTH(REPLACE(select_investors, ',', '')) >= numbers.n - 1
+)
+SELECT investor, COUNT(*) AS num_investments
+FROM investor_list
+GROUP BY investor
+ORDER BY num_investments DESC
+LIMIT 10;
+-- Accel, Tiger Global Management and Andreessen Horowitz
+
+-- List the unicorn companies with the highest valuation
+SELECT company, numeric_valuation
+FROM unicorns
+ORDER BY numeric_valuation DESC
+LIMIT 10;
+-- Bytedance, SpaceX and SHEIN 
+
+-- Count the number of unicorn companies without funding
+SELECT COUNT(*) AS unicorns_without_funding
+FROM unicorns
+WHERE numeric_funding = 0;
+-- There's only 1 Unicorn Company without funding
+
+-- Count the number of companies that did not disclose their funding
+SELECT COUNT(*)
+FROM unicorns
+WHERE numeric_funding IS NULL;
+-- 12 companies didnt disclose their funding
+
+-- The oldest unicorn company in the dataset
+SELECT company, industry, year_founded
+FROM unicorns
+ORDER BY year_founded ASC
+LIMIT 3;
+-- Otto Bock Healthcare, Promasidor Holdings and FIve Star Business Finance
+
+-- The newest unicorn companies in the dataset
+SELECT company, industry, year_founded
+FROM unicorns
+ORDER BY year_founded DESC
+LIMIT 3;
+-- Candy.com, Yuga Labs and Flink Food
+
+-- Average valuation of companies by year founded
+WITH year_valuations AS (
+    SELECT year_founded, AVG(numeric_valuation) AS avg_valuation
+    FROM unicorns
+    GROUP BY year_founded
+)
+SELECT * 
+FROM year_valuations
+ORDER BY year_founded;
+
+
